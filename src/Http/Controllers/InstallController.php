3e5cbc7c398f9c6b843348d67c914e43e27d44fa
3m1n3nc3 <mygames.ng@gmail.com>
2022-12-28 08:52:30 +0100

    Fix missing Artisan inmport in InstallController

--- src/Http/Controllers/InstallController.php
+++ src/Http/Controllers/InstallController.php
@@ -4,6 +4,7 @@ namespace Sbit\WebInstaller\Http\Controllers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
